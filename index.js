31104ffbbe4bd49eb989a85b51a259b480f8161c
fransilandia <mafca21@gmail.com>
2024-01-14 17:47:33 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+// function hello() {
+//  console.log('Hola Mundo');
+//  console.log('trabajando con js')  
+// }
+// hello()
+
+
+// function hello() {
+//     return 'Hola mundo'
+// }
+
+// //const result = hello()
+// console.log(hello());
+
+
+// function hello() {
+//     return function () {
+//         return 'hola mundo'
+//     }
+// }
+// console.log(hello()())
+
+// function hello (name) {
+//     return 'Hola ' + name;
+// }
+
+// console.log(hello('Josefina'))
+
+function add (x , y) {
+    return x + y;
+}
+console.log(add(10, 20));
